ea211dcf44869ffd9ac0bdc84f22e329fe4aa820
aleksandramitrovic02 <mitrovicaleksandra2000@gmail.com>
2023-12-28 20:28:21 +0100

    Geografska oblast klasa

---
+++ ProjekatERS/Comon/Model/GeografskaOblast.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comon.Model
+{
+    public class GeografskaOblast
+    {
+        public String Ime { get; set; }
+
+        public int Sifra { get; set; }
+
+        public GeografskaOblast(string ime, int sifra)
+        {
+            Ime = ime;
+            Sifra = sifra;
+        }
+
+        public GeografskaOblast() { }
+
+        public override string ToString()
+        {
+            return $"{Ime},{Sifra}";
+        }
+    }
+}
