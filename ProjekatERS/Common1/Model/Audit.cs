94ba08ffb9388f10f0bd97b6416a4d9000f413c3
aleksandramitrovic02 <mitrovicaleksandra2000@gmail.com>
2023-12-28 16:09:44 +0100

    Audit klasa

---
+++ ProjekatERS/Common1/Model/Audit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common1.Model
+{
+    public class Audit
+    {
+        public Audit(DateTime vremeUcitavanja, string imeFajla, string lokacija, int brojRedova)
+        {
+            VremeUcitavanja = vremeUcitavanja;
+            ImeFajla = imeFajla;
+            Lokacija = lokacija;
+            BrojRedova = brojRedova;
+        }
+
+        public Audit() { }
+
+        public override string ToString()
+        {
+            return $"{VremeUcitavanja},{ImeFajla},{Lokacija},{BrojRedova}";
+        }
+
+        public DateTime VremeUcitavanja { get; set; }
+
+        public String ImeFajla { get; set; }
+
+        public String Lokacija { get; set; }
+
+        public int BrojRedova { get; set; }
+    }
+}
