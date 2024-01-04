6f56b264b003a8ba555b6c2db64af77bdc899b3c
aleksandramitrovic02 <mitrovicaleksandra2000@gmail.com>
2024-01-04 18:29:27 +0100

    Servis klasa

---
+++ ProjekatERS/BazaPodataka/Servis.cs
@@ -0,0 +1,23 @@
+﻿using Comon;
+using Comon.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BazaPodataka
+{
+    public class Servis : IEvidencija
+    {
+        public void Audit(Audit a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrognoziranaIPotrosena(Potrosnja p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
