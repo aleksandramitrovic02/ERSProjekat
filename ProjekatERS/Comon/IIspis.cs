c44766bcd2a4f940ff11059957d99753cfeb5456
Isidora Vujasin <isidoravujasin9@gmail.com>
2024-01-11 09:41:03 +0100

    interfejs IIspis

---
+++ ProjekatERS/Comon/IIspis.cs
@@ -0,0 +1,16 @@
+﻿using Comon.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comon
+{
+    [ServiceContract]
+    public interface IIspis
+    {
+        List<List<Potrosnja>> Izracunaj(DateTime datum, string GeoPodrucje);
+    }
+}
