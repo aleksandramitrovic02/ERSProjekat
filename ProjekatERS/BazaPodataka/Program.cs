143866aa5d3ebe6860e10c2f8a54611ea4f571be
Isidora Vujasin <isidoravujasin9@gmail.com>
2024-01-11 10:02:45 +0100

    pokretanje servera

--- ProjekatERS/BazaPodataka/Program.cs
+++ ProjekatERS/BazaPodataka/Program.cs
@@ -18,17 +18,37 @@ namespace BazaPodataka
 
             //Audit a = new Audit(new DateTime(2022,10,10),"akjhsd","ashjdkk",10);
 
-           /* using (ServiceHost host = new ServiceHost(typeof(Server)))
-            {
-                host.Open();
-                Console.WriteLine("Servis je uspesno pokrenut");
-                Console.ReadKey();
-            }*/
+            /* using (ServiceHost host = new ServiceHost(typeof(Server)))
+             {
+                 host.Open();
+                 Console.WriteLine("Servis je uspesno pokrenut");
+                 Console.ReadKey();
+             }*/
 
-            
+            ServiceHost host = new ServiceHost(typeof(Server));
+
+            host.AddServiceEndpoint(typeof(IEvidencija),
+                 new NetTcpBinding(),
+               new Uri("net.tcp://localhost:4000/IEvidencija"));
+            host.Open();
+            Console.WriteLine("Servis1 je uspesno pokrenut");
+
+
+            ServiceHost host1 = new ServiceHost(typeof(ServisIspis));
+
+            host1.AddServiceEndpoint(typeof(IIspis),
+             new NetTcpBinding(),
+           new Uri("net.tcp://localhost:4001/IIspis"));
+
+            host1.Open();
+            Console.WriteLine("Servis2 je uspesno pokrenut");
+            Console.ReadKey();
 
 
             Console.Read();
+
+
+          
 
         }
     }
